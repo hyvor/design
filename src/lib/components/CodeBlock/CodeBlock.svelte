b95931afd2b30a7d628b1fdf071e715a71d48fb8
SupunKavinda <supunkavinda1125@gmail.com>
2023-10-09 02:56:26 +0200

    codeblock update

--- src/lib/components/CodeBlock/CodeBlock.svelte
+++ src/lib/components/CodeBlock/CodeBlock.svelte
@@ -7,45 +7,8 @@
         'html' | 'js' | 'css' | 'svelte'
         = 'html';
 
-    // @ts-ignore
-    import Prism from 'prismjs';
-    import 'prism-svelte';
-
-    function getCode() {
-
-        let ret = code;
-
-        // remove the first empty line
-        ret = ret.replace(/^[^\S\r\n]*\n/, "");
-        // remove the last empty line
-        ret = ret.replace(/\n[^\S\r\n]*$/, "");
-
-        let lines = ret.split("\n");
-        let indent : null | number = null; // number of spaces to remove from each line
-
-        lines = lines.map(line => {
-            if (indent === null) {
-                // find the indent
-                const match = line.match(/^(\s*)/);
-                indent = match ? match[1].length : 0;
-            }
-
-            if (line.substring(0, indent).trim() !== "") {
-                return line;
-            }
-
-            // remove the indent
-            line = line.substring(indent);
-
-            return line;
-        });
-
-        ret = lines.join("\n");
-
-        return Prism.highlight(ret, Prism.languages[language], language);
-
-    }
+    import getCode from './prism.js';
 
 </script>
 
-<pre class="language-{language}"><code>{@html getCode()}</code></pre>
+<pre class="language-{language}"><code>{@html getCode(code, language)}</code></pre>

ec805fbea1075e6c5f7aad1744b761cd6794de08
IshiniAvindya <ishiniavindya2000@gmail.com>
2023-11-23 06:40:30 +0100

    fixes #4

--- src/lib/components/Divider/Divider.svelte
+++ src/lib/components/Divider/Divider.svelte
@@ -3,18 +3,52 @@
     export let height : number = 1;
     export let width : number = 100;
     export let margin : number = 0;
+    export let align: 'start' | 'center' | 'end' = 'center';
 </script>
-
-<div 
+<!-- <div 
     class="line"
     style="
         background-color: {color};
         height: {height}px;
         width: {width}%;
         margin: {margin}px 0;
+        align: {align};
     "
 />
 
 <style>
    
+</style> -->
+
+<!-- <div 
+    class="line"
+    style="
+        background-color: {color};
+        height: {height}px;
+        width: {width}%;
+        margin-top: {margin}px;
+        margin-bottom: {margin}px;
+        margin-left: {align === 'end' ? 'auto' : '0'};
+        margin-right: {align === 'start' ? 'auto' : '0'};
+    "
+/> -->
+
+<div
+    class="line"
+    style="
+    background-color: {color};
+        height: {height}px;
+        width: {width}%;
+        margin-top: {margin}px;
+        margin-bottom: {margin}px;
+        margin-left: {align === 'center' || align === 'end' ? 'auto' : 0};
+        margin-right: {align === 'center' ? 'auto' : 0};
+        display: flex;
+    "
+/>
+
+<style>
+    .line {
+        display: block;
+    }
 </style>

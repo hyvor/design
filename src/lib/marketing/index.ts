cf1a985ab162f228be05b0a33f7b9910f4e4165f
IshiniAvindya <ishiniavindya2000@gmail.com>
2023-12-18 16:12:14 +0100

    fixes #26

--- src/lib/marketing/index.ts
+++ src/lib/marketing/index.ts
@@ -8,3 +8,6 @@ export { default as DocsNav } from './Docs/Nav/Nav.svelte';
 export { default as DocsNavItem } from './Docs/Nav/NavItem.svelte';
 export { default as DocsNavCategory } from './Docs/Nav/NavCategory.svelte';
 export { default as DocsContent } from './Docs/Content/Content.svelte';
+
+export { default as Footer } from './Footer/Footer.svelte';
+export { default as FooterLinkList } from './Footer/FooterLinkList.svelte';

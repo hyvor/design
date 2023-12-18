fdaf93d7db735a41d7ac59126b0ce40e8bdb7d8f
IshiniAvindya <ishiniavindya2000@gmail.com>
2023-12-18 15:19:39 +0100

    fixes #25

--- src/lib/components/CodeBlock/CodeBlock.svelte
+++ src/lib/components/CodeBlock/CodeBlock.svelte
@@ -26,3 +26,28 @@
 </script>
 
 <pre class="language-{languageCode} hljs"><code>{@html getCode(code, languageCode)}</code></pre>
+
+<style>
+    /*styles for CodeBlock component */
+    pre {
+        text-align: left;
+        white-space: pre;
+        word-spacing: normal;
+        word-break: normal;
+        word-wrap: normal;
+        overflow: auto;
+        border-radius: 20px;
+        padding: 20px;
+        line-height: 1.1;
+    }
+    pre code {
+        all: unset;
+        font-family: Consolas,Monaco,Andale Mono,Ubuntu Mono,monospace;
+        font-size: 14px;
+        line-height: 1.5;
+        tab-size: 4;
+        hyphens: none;
+    }
+
+
+</style>

c7052c0bc6cd1a3bcf9d699a24ad136a29a215a5
Supun Wimalasena <supun@hyvor.com>
2025-02-03 19:11:30 +0100

    emoji picker wip

---
+++ src/lib/components/EmojiPicker/EmojiPicker.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { getData } from "./emoji.js";
+
+    onMount(() => {
+
+        getData().then((data) => {
+            console.log(data);
+        });
+
+    });
+
+</script>
+
+
+<div class="picker hds-box">
+
+    // 
+
+</div>
+

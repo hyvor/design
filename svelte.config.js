147bf8707efe2ee7f81f1a28267ef4d299167ddf
IshiniAvindya <ishiniavindya2000@gmail.com>
2025-11-08 10:52:05 +0100

    mdsvex added

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,4 +18,10 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+  extensions: ['.svelte', '.svx', '.md'],
+  preprocess: mdsvex({ extensions: ['.svx', '.md'] }),
+  config,
+}
+
+// export default config;

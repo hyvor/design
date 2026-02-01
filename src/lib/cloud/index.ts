437e251d9220258583966d3b084e2432fdb9a872
Supun Wimalasena <supun@hyvor.com>
2026-02-01 04:09:08 +0100

    export

--- src/lib/cloud/index.ts
+++ src/lib/cloud/index.ts
@@ -13,3 +13,4 @@ export { default as HyvorBar } from './HyvorBar/HyvorBar.svelte';
 export { bar as hyvorBar } from './HyvorBar/bar.js';
 
 export { default as ResourceCreator } from './ResourceCreator/ResourceCreator.svelte';
+export { default as OrganizationMemberSearch } from './OrganizationMembers/OrganizationMembersSearch.svelte';

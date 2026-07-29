0e9f11b9ded5c94c3aae83e05bb086c1b878a668
Ishini Senanayake <ishiniavindya2000@gmail.com>
2026-07-29 14:13:31 +0200

    fix: nested list padding fix
    nested lists padding fix in docs closes #312

--- src/lib/marketing/Document/Document.svelte
+++ src/lib/marketing/Document/Document.svelte
@@ -81,12 +81,15 @@
 			margin-top: 20px;
 		}
 
-		content :global(ul) {
-			margin-top: 20px;
-		}
-
+		content :global(ul),
 		content :global(ol) {
 			margin-top: 20px;
+			margin-bottom: 0;
+		}
+
+		content :global(li ul),
+		content :global(li ol) {
+			margin-top: 0;
 		}
 
 		content :global(hr) {

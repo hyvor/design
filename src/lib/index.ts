c852c735a2cc847f722a51afa84a99414edc82ce
Supun Wimalasena <supun@hyvor.com>
2026-09-10 14:29:42 +0200

    fix: inline styles in Base

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-import './index.css';
-import '@fontsource/readex-pro/400.css';
-import '@fontsource/readex-pro/600.css';
-import '@fontsource/source-serif-4/400.css';
-import '@fontsource/source-serif-4/600.css';
